34530586099a9c581d6da63749817020e94d1da9
Alex <alex@Miracle>
2013-07-11 10:10:25 +0800

    groovy class and class variables

---
+++ src/Person.groovy
@@ -0,0 +1,24 @@
+
+class Person {
+// In Groovy all fields of a class have by default the access modifier "private" 
+// and Groovy provides automatically getter and setter methods for the fields
+  String firstName
+  String lastName
+  int age
+  def address
+  
+  static void main(def args) {
+    Person p = new Person()
+    // Use the generated access methods
+    p.setFirstName("Alex")
+    // This will still use the generated access method, it is not a direct access!
+    p.lastName = "Chen" 
+	p.age  = 24;
+    p.address = ("Liftcore");
+    println(p.firstName + " " + p.lastName + " "+p.age);
+    // Use the generated constructor
+    p = new Person(firstName: "Tom", lastName:"Cat");
+    println(p.firstName + " " + p.lastName);
+  }
+
+}
