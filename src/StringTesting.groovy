879b1b2094c219b9495f88405a1c51dc19532c69
Alex <alex@Miracle>
2013-07-11 10:24:18 +0800

    groovy String and GString usage

---
+++ src/StringTesting.groovy
@@ -0,0 +1,15 @@
+
+class StringTesting {
+// Strings which are quoted in by "" are so-called GStrings (short for Groovy Strings). 
+// In GStrings you can directly use variables which will then be evaluated and included in the text.
+  public static void main(String[] args) {
+    def name = "Groovy"
+    def s1 = "Hello $name" // $name will be replaced
+    def s2 = 'Hello $name' // $name will not be replaced
+    println s1
+    println s2
+    println s1.getClass().getName();
+    println s2.getClass().getName();
+  }
+
+}
